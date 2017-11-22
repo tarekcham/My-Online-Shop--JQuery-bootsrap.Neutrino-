f332482da3072555024f3dfc82c14f0717c34d39
Tarek Alkhatib <tarek.alkhatib1@gmail.com>
2017-11-22 13:04:19 +0100

    add navbar

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -3,11 +3,12 @@
 module.exports = {
   use: [
     ['neutrino-preset-airbnb-base'],
+    ['neutrino-middleware-styles-loader'],
     [
       'neutrino-preset-web',
       {
         html: {
-          title: 'My App'
+          title: 'My App',
         }
       }
     ],
