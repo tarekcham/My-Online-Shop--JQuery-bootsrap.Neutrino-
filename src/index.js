8459d22ec6e1b3122c46b0b7085b0679f3ab7a55
Tarek Alkhatib <tarek.alkhatib1@gmail.com>
2017-11-29 10:43:29 +0100

    carousel edit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
 import navbar from './templates/navbar.html';
+import carousel from './templates/carousel.html';
 
 const pictures = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg'];
 
@@ -33,132 +34,6 @@ const grids = `
     <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[5])}</div>
   </div>
 </div>
-`;
-
-const carousel = `
- <div  class="carousel-backColor" >
-   <div id="demo" class="carousel slide" data-ride="carousel">
-
-      <!-- Indicators -->
-      <ul class="carousel-indicators" >
-        <li  data-target="#demo" data-slide-to="0" class="active"></li>
-        <li data-target="#demo" data-slide-to="1"></li>
-        <li data-target="#demo" data-slide-to="2"></li>
-        <li data-target="#demo" data-slide-to="3"></li>
-      </ul>
-
-      <!-- The slideshow -->
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-
-
-            <div class='container'>
-
-              <div class=row>
-                <div class= "col-lg-6">
-                  <h1> Slide 1 </h1>
-                  <p>  חתול הבית (שם מדעי: Felis silvestris catus) הוא יונק טורף מבוית,
-                    מהסוג חתול ממשפחת החתוליים. החתול נפוץ בכל יבשות העולם מלבד באנטארקטיקה.
-                    משערים כי מוצאו מחתול הבר. החתול הוא טורף לילי הצד מכרסמים, ציפורים ודגים.
-                    נכון ל-2009, החתול הוא חיית המחמד הנפוצה ביותר בעולם,
-                    ומספר החתולים הביתיים נאמד בכ-600 מיליון. 
-                    החתול חי בדרך כלל בבדידות, למעט אינטראקציה מזדמנת עם בני מינו.
-                  </p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                <div class= "col-lg-6">
-                  <img  class="carouselImag" src="./static/cat1.jpg" alt="Los Angeles">
-                </div>
-                
-              </div>
-            </div>
-        </div>
-
-
-        <div class="carousel-item">
-          <div class='container'>
-
-              <div class=row>
-                
-                <div class= "col-lg-6">
-                  <img  class="carouselImag" src="./static/cat2.jpg" alt="Los Angeles">
-                </div>
-                <div class= "col-lg-6">
-                  <h1> Slide 1 </h1>
-                  <p>  חתול הבית (שם מדעי: Felis silvestris catus) הוא יונק טורף מבוית,
-                    מהסוג חתול ממשפחת החתוליים. החתול נפוץ בכל יבשות העולם מלבד באנטארקטיקה.
-                    משערים כי מוצאו מחתול הבר. החתול הוא טורף לילי הצד מכרסמים, ציפורים ודגים.
-                    נכון ל-2009, החתול הוא חיית המחמד הנפוצה ביותר בעולם,
-                    ומספר החתולים הביתיים נאמד בכ-600 מיליון. 
-                    החתול חי בדרך כלל בבדידות, למעט אינטראקציה מזדמנת עם בני מינו.
-                  </p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                
-              </div>
-            </div>
-        
-        </div>
-        <div class="carousel-item">
-          <div class='container'>
-
-              <div class=row>
-                <div class= "col-lg-6">
-                  <h1> Slide 1 </h1>
-                  <p>  חתול הבית (שם מדעי: Felis silvestris catus) הוא יונק טורף מבוית,
-                    מהסוג חתול ממשפחת החתוליים. החתול נפוץ בכל יבשות העולם מלבד באנטארקטיקה.
-                    משערים כי מוצאו מחתול הבר. החתול הוא טורף לילי הצד מכרסמים, ציפורים ודגים.
-                    נכון ל-2009, החתול הוא חיית המחמד הנפוצה ביותר בעולם,
-                    ומספר החתולים הביתיים נאמד בכ-600 מיליון. 
-                    החתול חי בדרך כלל בבדידות, למעט אינטראקציה מזדמנת עם בני מינו.
-                  </p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                <div class= "col-lg-6">
-                  <img  class="carouselImag" src="./static/cat3.jpg" alt="Los Angeles">
-                </div>
-                
-              </div>
-            </div>
-        
-        </div>
-      
-      <div class="carousel-item">
-          <div class='container'>
-
-              <div class=row>
-                
-                <div class= "col-lg-6">
-                  <img  class="carouselImag" src="./static/cat4.jpg" alt="Los Angeles">
-                </div>
-                <div class= "col-lg-6">
-                  <h1> Slide 1 </h1>
-                  <p>  חתול הבית (שם מדעי: Felis silvestris catus) הוא יונק טורף מבוית,
-                    מהסוג חתול ממשפחת החתוליים. החתול נפוץ בכל יבשות העולם מלבד באנטארקטיקה.
-                    משערים כי מוצאו מחתול הבר. החתול הוא טורף לילי הצד מכרסמים, ציפורים ודגים.
-                    נכון ל-2009, החתול הוא חיית המחמד הנפוצה ביותר בעולם,
-                    ומספר החתולים הביתיים נאמד בכ-600 מיליון. 
-                    החתול חי בדרך כלל בבדידות, למעט אינטראקציה מזדמנת עם בני מינו.
-                  </p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                
-              </div>
-            </div>
-        
-        </div>
-      </div>
-
-      <!-- Left and right controls -->
-      <a  class="carousel-control-prev" href="#demo" data-slide="prev">
-        <span  class="carousel-control-prev-icon"></span>
-      </a>
-      <a class="carousel-control-next" href="#demo" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </a>
-
-    </div>
-  </div>
 `;
 
 const catCards = `
