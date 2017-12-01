387897eee145485a2572a9c12cd5a3ec32a50ff5
Tarek Alkhatib <tarek.alkhatib1@gmail.com>
2017-12-01 16:16:24 +0100

    make product-grid

--- src/index.js
+++ src/index.js
@@ -1,109 +1,38 @@
 import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
-import navbar from './templates/navbar.html';
-import carousel from './templates/carousel.html';
+import navbarTemplate from './templates/navbar.html';
+import mkCarousel from './carousel';
+import mkProductsGrid from './products-grid';
 
-const pictures = ['1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg'];
 
-function mkCard(img) {
-  return `
-<div class="card-group">
-  <div class="card">
-    <img class="card-img-top" src="static/${img}" alt="Card image cap">
-    <div class="card-block">
-      <h4 class="card-title">Card title</h4>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-</div>
- `;
+function updateNavbar(categories) {
+  const $navbarNav = $('.navbar-nav').empty();
+  categories.forEach((category) => {
+    $navbarNav.append(`<li class="nav-item">
+      <a class="nav-link" href="#">${category.name}</a>
+    </li>`);
+  });
 }
 
-const grids = `
-<div class="container">
-  <div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[0])}</div>
-    <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[1])}</div>
-    <div class="clearfix hidden-sm-up"></div>
-    <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[2])}</div>
-    <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[3])}</div>
-    <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[4])}</div>
-    <div class="col-lg-4 col-md-6 col-sm-12">${mkCard(pictures[5])}</div>
-  </div>
-</div>
-`;
-
-const catCards = `
-<div class="container">
-  <div class="row">
-    <div class="col-lg-3 col-md-6 col-sm-12">
-
-      <div class="card-group">
-        <div class="card">
-          <img class="card-img-top" src="./static/cat1.jpg" alt="Card image cap">
-          <div class="card-block">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-     </div>
-    </div>
-    </div>
-
-    <div class="col-lg-3 col-md-6 col-sm-12">
-
-      <div class="card-group">
-        <div class="card">
-          <img class="card-img-top" src="./static/cat2.jpg" alt="Card image cap">
-          <div class="card-block">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-     </div>
-    </div>
-    </div>
-
-    <div class="col-lg-3 col-md-6 col-sm-12">
-
-      <div class="card-group">
-        <div class="card">
-          <img class="card-img-top" src="./static/cat3.jpg" alt="Card image cap">
-          <div class="card-block">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-     </div>
-    </div>
-    </div>
-
-    <div class="col-lg-3 col-md-6 col-sm-12">
-
-      <div class="card-group">
-        <div class="card">
-          <img class="card-img-top" src="./static/cat4.jpg" alt="Card image cap">
-          <div class="card-block">
-            <h4 class="card-title">Card title</h4>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-     </div>
-    </div>
-    </div>
-
-  </div>
-</div>
-`;
-
 $(() => {
-  $('#root').append(navbar).append(grids).append(carousel)
-    .append(catCards);
+  $('#root').append(navbarTemplate);
+
+  $.ajax('./static/categories.json')
+    .done((categories) => {
+      const $carousel = mkCarousel(categories);
+      $('#root').append($carousel);
+      // because the HTML of the carousel
+      // is added after the page loads,
+      // we need to initialize the
+      // Bootstrap carousel ourselves
+      $carousel.carousel();
+      updateNavbar(categories);
+    });
+
+  $.ajax('./static/products.json')
+    .done((products) => {
+      const $productsGrid = mkProductsGrid(products);
+      $('#root').append($productsGrid);
+    });
 });
